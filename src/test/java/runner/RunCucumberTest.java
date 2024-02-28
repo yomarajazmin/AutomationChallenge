14f6a79dc9ab4e019e6456cb9c3e1204e3a9cf10
yomarajazmin <yomarajazmin@gmail.com>
2024-02-28 03:30:45 -0400

    fixing report format

--- src/test/java/runner/RunCucumberTest.java
+++ src/test/java/runner/RunCucumberTest.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 
 @RunWith( Cucumber.class )
-@CucumberOptions(monochrome = false, plugin = { "html:target/cucumber-html-report", "json:target/cucumber.json",
+@CucumberOptions(monochrome = false, plugin = { "html:target/cucumber-html-report.html", "json:target/cucumber.json",
         "pretty:target/cucumber-pretty.txt", "usage:target/cucumber-usage.json",
         "junit:target/cucumber-results.xml" }, features = "src/test/resources/features", glue = { "utils.steps" })
 
